addf5025862ad47b5e5aa1430af73c88ab219c24
Axel Sanner <axel@xeneta.com>
2015-04-08 23:29:20 +0200

    commiting changes

--- src/astar_test.cpp
+++ src/astar_test.cpp
@@ -1,6 +1,59 @@
+#include <stdio.h>
+#include "uthash.h"
 
+int items_added = 0;
 
+int path[10][10] = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+};
 
+struct Points {
+    int x;
+    int y;
+    int id;
+    int fcost;
+    int hcost;
+    int gcost;
+    struct Points *parent;
+    UT_hash_handle hh;
+};
 
+struct Points *point = NULL;
 
-// Test
+void add_user(int user_id, int x, int y) {
+    struct Points *p = NULL;
+
+    p = (struct Points*)malloc(sizeof(struct Points));
+    p->id = user_id;
+    p->x = 22;
+    p->y = 22;
+    HASH_ADD_INT( point, id, p );  /* id: name of key field */
+}
+
+void add_items() {
+    int i,j;
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+            printf("%i ", path[i][j]);
+            add_user(i+j, j, i);
+            items_added++;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    printf("Hello world\n");
+
+    add_items();
+    return 0;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,97 @@
+#include <stdio.h>
+#include "uthash.h"
 #include <Python.h>
+
+int items_added = 0;
+
+long int map[200];
+
+int path[10][10] = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1},
+};
+
+struct Points {
+    int x;
+    int y;
+    int id;
+    int fcost;
+    int hcost;
+    int gcost;
+    struct Points *parent;
+    UT_hash_handle hh;
+};
+
+struct Points *point = NULL;
+
+void add_user(int user_id, int x, int y) {
+    struct Points *p = NULL;
+
+    p = (struct Points*)malloc(sizeof(struct Points));
+    p->id = user_id;
+    p->x = 22;
+    p->y = 22;
+    HASH_ADD_INT( point, id, p );  /* id: name of key field */
+}
+
+void add_items() {
+    int i,j;
+    for(i=0; i<10; i++){
+        for(j=0; j<10; j++){
+            printf("%i ", path[i][j]);
+            add_user(i+j, j, i);
+            items_added++;
+        }
+        printf("\n");
+    }
+}
+
+
+static PyObject* set_map(PyObject* self, PyObject* args) {
+
+    PyObject *obj;
+
+    if (!PyArg_ParseTuple(args, "O", &obj)) {
+      // error
+    }
+
+    PyObject *iter = PyObject_GetIter(obj);
+    if (!iter) {
+      // error not iterator
+    }
+
+    int i = 0;
+
+    while (true) {
+      PyObject *next = PyIter_Next(iter);
+      if (!next) {
+        // nothing left in the iterator
+        break;
+      }
+
+      if (!PyInt_Check(next)) {
+        // error, we were expecting a floating point value
+      }
+
+      long int foo = PyInt_AsLong(next);
+      // do something with foo
+      map[i] = foo;
+    }
+	return Py_BuildValue("i", 0);
+}
+
+static PyObject* get_map(PyObject* self, PyObject* args){
+
+	Py_RETURN_NONE;
+}
 
 static PyObject* say_hello(PyObject* self, PyObject* args)
 {
@@ -30,6 +123,8 @@ static PyMethodDef HelloMethods[] =
 {
 	{ "say_hello", say_hello, METH_VARARGS, "Greet somebody." },
 	{ "ret4", ret4, METH_VARARGS, "Return random" },
+	{ "set_map", set_map, METH_VARARGS, "Set a map" },
+	{ "get_map", get_map, METH_VARARGS, "Set a map" },
 	{ NULL, NULL, 0, NULL }
 };
 
