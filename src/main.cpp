d472876d15aa1a5b5d98256737a76be289c8bb89
Axel Sanner <axel@xeneta.com>
2015-04-11 17:34:27 +0200

    Made a my own type

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "uthash.h"
 #include <Python.h>
+#include "structmember.h"
 
 int items_added = 0;
 
@@ -18,6 +19,12 @@ int path[10][10] = {
     {1,1,1,1,1,1,1,1,1,1},
     {1,1,1,1,1,1,1,1,1,1},
 };
+
+typedef struct {
+    PyObject_HEAD
+    PyObject * dict;
+    int count;
+} CDict;
 
 struct Points {
     int x;
@@ -53,6 +60,7 @@ void add_items() {
         printf("\n");
     }
 }
+
 
 
 static PyObject* set_map(PyObject* self, PyObject* args) {
@@ -133,16 +141,115 @@ static PyObject* ret4(PyObject* self, PyObject* args) {
 	//return Py_BuildValue("i", 4);
 }
 
+
+static PyObject *CDict_set(CDict *self, PyObject *args)
+{
+   const char *key;
+   PyObject *value;
+    
+   if (!PyArg_ParseTuple(args, "sO:set", &key, &value)) {
+      return NULL;
+   }
+
+   if (PyDict_SetItemString(self->dict, key, value) < 0) {
+      return NULL;
+   }
+    
+   self->count++;
+    
+   return Py_BuildValue("i", self->count);
+}
+
+static int CDict_init(CDict *self, PyObject *args, PyObject *kwds) {
+    self->dict = PyDict_New();
+    self->count = 0;
+    return 0;
+}
+
+static void CDict_dealloc(CDict *self) {
+    Py_XDECREF(self->dict);
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static PyMemberDef CDict_members[] = 
+{
+    {"dict", T_OBJECT, offsetof(CDict, dict), 0, "HABBLAHABBLA"},
+    {"count", T_INT, offsetof(CDict, count), 0, "HIBBLOGIBBLO"},
+    { NULL }
+};
+
+static PyMethodDef
+CDict_methods[] = {
+   { "set", (PyCFunction) CDict_set, METH_VARARGS, "Set a key and increment the count." },
+   { NULL }
+};
+
 static PyMethodDef HelloMethods[] =
 {
 	{ "say_hello", say_hello, METH_VARARGS, "Greet somebody." },
 	{ "ret4", ret4, METH_VARARGS, "Return random" },
 	{ "set_map", set_map, METH_VARARGS, "Set a map" },
 	{ "get_map", get_map, METH_VARARGS, "Set a map" },
+    { "set", (PyCFunction) CDict_set, METH_VARARGS, "Set a key and increment the count." },
 	{ NULL, NULL, 0, NULL }
+};
+
+static PyTypeObject
+CDictType = {
+   PyObject_HEAD_INIT(NULL)
+   0,                         /* ob_size */
+   "CDict",               /* tp_name */
+   sizeof(CDict),         /* tp_basicsize */
+   0,                         /* tp_itemsize */
+   (destructor)CDict_dealloc, /* tp_dealloc */
+   0,                         /* tp_print */
+   0,                         /* tp_getattr */
+   0,                         /* tp_setattr */
+   0,                         /* tp_compare */
+   0,                         /* tp_repr */
+   0,                         /* tp_as_number */
+   0,                         /* tp_as_sequence */
+   0,                         /* tp_as_mapping */
+   0,                         /* tp_hash */
+   0,                         /* tp_call */
+   0,                         /* tp_str */
+   0,                         /* tp_getattro */
+   0,                         /* tp_setattro */
+   0,                         /* tp_as_buffer */
+   Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /* tp_flags*/
+   "CDict object",        /* tp_doc */
+   0,                         /* tp_traverse */
+   0,                         /* tp_clear */
+   0,                         /* tp_richcompare */
+   0,                         /* tp_weaklistoffset */
+   0,                         /* tp_iter */
+   0,                         /* tp_iternext */
+   CDict_methods,         /* tp_methods */
+   CDict_members,         /* tp_members */
+   0,                         /* tp_getset */
+   0,                         /* tp_base */
+   0,                         /* tp_dict */
+   0,                         /* tp_descr_get */
+   0,                         /* tp_descr_set */
+   0,                         /* tp_dictoffset */
+   (initproc)CDict_init,  /* tp_init */
+   0,                         /* tp_alloc */
+   0,                         /* tp_new */
 };
 
 PyMODINIT_FUNC initastar(void)
 {
-	(void)Py_InitModule("astar", HelloMethods);
+    PyObject* mod;
+    (void)Py_InitModule("astar", HelloMethods);
+    mod = Py_InitModule("astar", CDict_methods);
+    // Fill in some slots in the type, and make it ready
+    CDictType.tp_new = PyType_GenericNew;
+    if (PyType_Ready(&CDictType) < 0) {
+        return;
+    }
+
+    // Add the type to the module.
+    Py_INCREF(&CDictType);
+    PyModule_AddObject(mod, "CDict", (PyObject*)&CDictType);
+
 }
