9a5f4a71d435e136d8681aa1cc41f80fd80bd63d
Axel Sanner <axelsanner@gmail.com>
2015-04-13 00:09:47 +0200

    a little change to windows build script

--- make.bat
+++ make.bat
@@ -1,3 +1,28 @@
-c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
+@ECHO OFF
+:: "c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 
-cl /LD src/main.cpp /Ic:\Python27\include c:\Python27\libs\python27.lib /link/out:astar.pyd
+:: To use cl.exe directly uncomment below
+:: cl /LD src/*.cpp /Ic:\Python27\include c:\Python27\libs\python27.lib /link/out:astar.pyd
+
+:: To use python setup.py build option you need to have either VS2008 or if using any other VS, set a envir var
+:: PS: If your'e using VS2008 you don't need to set envir var.
+
+:: For VS2013 uncomment below
+SET VS90COMNTOOLS=%VS120COMNTOOLS%
+
+:: For VS2012
+:: SET VS90COMNTOOLS=%VS110COMNTOOLS%
+
+:: For VS2010
+:: SET VS90COMNTOOLS=%VS100COMNTOOLS%
+
+
+@ECHO ON
+python setup.py build
+@ECHO OFF
+if exist "astar.pyd" (
+    del astar.pyd
+)
+
+copy build\lib.win32-2.7\astar.pyd .
+@ECHO ON
