bb8a6ad05be4d0274c254360f2fecbfb07c24bf3
Axel Sanner <axel@xeneta.com>
2016-02-05 02:29:52 +0100

    pthreads not useable on windows

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from distutils.core import setup, Extension
 # To Build: python setup.py build_ext -i
 
 module1 = Extension('pathfinding',
-        include_dirs = ['/usr/local/include'],
-        libraries = ['pthread'],
-        sources = ['src/main.cpp', 'src/astar.cpp'])
+        include_dirs=['/usr/local/include'],
+        sources=['src/main.cpp', 'src/astar.cpp'])
 
 setup(name='PackageName',
         version='1.0',
